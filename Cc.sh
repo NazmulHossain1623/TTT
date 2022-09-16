d771740ca2eac95c130e737aba61498af287c705
NAZMUL HOSSAIN <112247853+NazmulHossain1623@users.noreply.github.com>
2022-09-16 22:01:25 +0600

    Create Cc.sh

---
+++ Cc.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#code_recorded_by_nazmul_hossain
+clear
+sleep 1
+echo ""
+echo ""
+echo " 
+
+███╗   ██╗ █████╗ ███████╗███╗   ███╗██╗   ██╗██╗     
+████╗  ██║██╔══██╗╚══███╔╝████╗ ████║██║   ██║██║     
+██╔██╗ ██║███████║  ███╔╝ ██╔████╔██║██║   ██║██║     
+██║╚██╗██║██╔══██║ ███╔╝  ██║╚██╔╝██║██║   ██║██║     
+██║ ╚████║██║  ██║███████╗██║ ╚═╝ ██║╚██████╔╝███████╗
+╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚══════╝
+                                                      
+" | lolcat
+
+
+
+
+echo ""
+sleep 1
+clear
+
+pkg install php -y
+pkg install openssh -y
+pkg install unzip -y
+pkg install curl -y
+sleep 1
+clear
+
+echo "Loading==> NAZMUL HOSSAIN" | lolcat
+sleep 1
+clear
+echo "Loading====> NAZMUL HOSSAIN" | lolcat
+sleep 1
+clear
+echo "Loading=======> NAZMUL HOSSAIN" | lolcat
+sleep 1
+clear
+echo " NAZMUL SAID COMMAND RUN DONE " | lolcat
+sleep 1
+clear
+sleep 4
+echo ""
+echo "
+███╗   ██╗ █████╗ ███████╗███╗   ███╗██╗   ██╗██╗     
+████╗  ██║██╔══██╗╚══███╔╝████╗ ████║██║   ██║██║     
+██╔██╗ ██║███████║  ███╔╝ ██╔████╔██║██║   ██║██║     
+██║╚██╗██║██╔══██║ ███╔╝  ██║╚██╔╝██║██║   ██║██║     
+██║ ╚████║██║  ██║███████╗██║ ╚═╝ ██║╚██████╔╝███████╗
+╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚══════╝ " | lolcat
+
+echo " FOLLOW MY FACEBOOK ACCOUNT 🔔 " | lolcat
+xdg-open https://www.facebook.com/NazmulHossain16239
+echo ""
+sleep 10
+echo " FOLLOW OUR PAGE" | lolcat
+echo " THANKS FOR FOLLOWING " | lolcat
+termux-open-url https://www.facebook.com/profile.php?id=100043316096491
+
+echo "
+
+▁▂▄▅▆▇█  𝗛𝗘𝗔𝗥 𝗪𝗘 𝗚𝗢 🥰  █▇▆▅▄▂▁
+▁▂▄▅▆▇█  THANKS FOR USING THIS TOOLS  █▇▆▅▄▂▁ "
+echo ""
+sleep 3
+echo "▁▂▄▅▆▇█ [  PREMIUM TOOLS RUNNING START  ] █▇▆▅▄▂▁ "
+sleep 2
+echo -e " \e[32m[✓] OK RESULTS SHOW THIS TYPE "
+echo -e " \e[31m[ × ] CP RESULTS SHOW THIS TYPE "
+sleep 1
+echo ""
+echo "_____________________________________________________________"
+sleep 15
+echo -e " \e[32m[✓] 🥰🥰🥰🥰🥰"
