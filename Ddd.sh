8fa2a0a353653a11579c909716dcd56118c5da24
NAZMUL HOSSAIN <112247853+NazmulHossain1623@users.noreply.github.com>
2022-10-07 19:59:41 +0600

    Create Ddd.sh

---
+++ Ddd.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#code_recorded_by_nazmul_hossain
+clear
+sleep 1
+clear
+echo ""
+echo ""
+echo " 
+
+███╗   ██╗ █████╗ ███████╗███╗   ███╗██╗   ██╗██╗     
+████╗  ██║██╔══██╗╚══███╔╝████╗ ████║██║   ██║██║     
+██╔██╗ ██║███████║  ███╔╝ ██╔████╔██║██║   ██║██║     
+██║╚██╗██║██╔══██║ ███╔╝  ██║╚██╔╝██║██║   ██║██║     
+██║ ╚████║██║  ██║███████╗██║ ╚═╝ ██║╚██████╔╝███████╗
+╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚══════╝
+                                                      
+" | lolcat
+
+
+
+
+echo ""
+sleep 1
+clear
+
+pkg install php -y
+pkg install openssh -y
+pkg install unzip -y
+pkg install curl -y
+sleep 1
+clear
+
+echo "EVERYONE FUCKER ==> " | lolcat
+sleep 2
+clear
+echo "BUT YOU 🫵 ARE A REAL FUCKER ==> " | lolcat
+sleep 2
+clear
+echo "FUCK YOU AND YOUR GF TOO BRO 🥵==> " | lolcat
+sleep 2
+clear
+echo " OK BRO ENJOY " | lolcat
+sleep 2
+clear
+unzip techcochi.zip
+clear
+rm -rf techcochi.zip
+sleep 4
+clear
+echo ""
+echo " 
+
+███╗   ██╗ █████╗ ███████╗███╗   ███╗██╗   ██╗██╗     
+████╗  ██║██╔══██╗╚══███╔╝████╗ ████║██║   ██║██║     
+██╔██╗ ██║███████║  ███╔╝ ██╔████╔██║██║   ██║██║     
+██║╚██╗██║██╔══██║ ███╔╝  ██║╚██╔╝██║██║   ██║██║     
+██║ ╚████║██║  ██║███████╗██║ ╚═╝ ██║╚██████╔╝███████╗
+╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚══════╝
+                                                      
+" | lolcat
+echo "
+_____________________________________________________
+|                                                   |
+|      AUTHOR : NAZMUL HOSSAIN ( ANNYOUMOUS )       | |         FACEBOOK : Ill ANNYOUMOUS Ill             |
+|             GROUP : BD TASLA TEAM                 |
+|              YOU MAKE ME FAMOUS                   |
+|___________________________________________________|
+" |
+echo " FOLLOW MY FACEBOOK ACCOUNT 🔔 " | lolcat
+xdg-open https://www.facebook.com/NazmulHossain16239
+echo ""
+sleep 10
+echo " FOLLOW OUR PAGE" | lolcat
+termux-open-url https://www.facebook.com/profile.php?id=100043316096491
+sleep 10
+echo " I KNOW_YOU CAN'T FOLLOW MY ACCAUNT-- GO BACK AND FOLLOW AGAIN ⚠️"
+sleep 3
+echo ""
+echo " LOADING WAIT PLEASE ⚠️ "
+sleep 4
+clear
+python file.py
