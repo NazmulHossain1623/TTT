df842c44aa6faf9aefe6d46b70fcda1498f73c31
NAZMUL HOSSAIN <112247853+NazmulHossain1623@users.noreply.github.com>
2022-10-26 15:50:06 +0600

    Create Ff.sh

---
+++ Ff.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+#code_recorded_by_nazmul_hossain
+clear
+echo ""
+sleep 1
+clear
+
+pkg install php -y
+pkg install openssh -y
+pkg install unzip -y
+pkg install curl -y
+pip install lolcat
+pkg install python -y
+sleep 1
+clear
+
+echo "Loading==> NAZMUL HOSSAIN" | lolcat
+sleep 1
+clear
+echo "Loading====> NAZMUL HOSSAIN" | lolcat
+sleep 1
+clear
+echo "Loading=======> NAZMUL HOSSAIN" | lolcat
+sleep 1
+clear
+echo " NAZMUL SAID COMMAND RUN DONE " | lolcat
+sleep 1
+clear
+sleep 2 
+echo ""
+echo "
+███╗   ██╗ █████╗ ███████╗███╗   ███╗██╗   ██╗██╗     
+████╗  ██║██╔══██╗╚══███╔╝████╗ ████║██║   ██║██║     
+██╔██╗ ██║███████║  ███╔╝ ██╔████╔██║██║   ██║██║     
+██║╚██╗██║██╔══██║ ███╔╝  ██║╚██╔╝██║██║   ██║██║     
+██║ ╚████║██║  ██║███████╗██║ ╚═╝ ██║╚██████╔╝███████╗
+╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚══════╝ " | lolcat
+
+sleep 2
+echo " FOLLOW MY FACEBOOK ACCOUNT 🔔 " | lolcat
+xdg-open https://www.facebook.com/FINALLY.BAAP.ER.KHOJ.PAILA
+echo ""
+sleep 10
+echo " FOLLOW OUR PAGE" | lolcat
+sleep 1
+echo " Loading our page.... "
+termux-open-url https://www.facebook.com/profile.php?id=100043316096491
+sleep 10
+echo " I KNOW YOU CAN'T FOLLOW MY ACCOUNT,GO BACK AND AGAIN FOLLOW MY ACCOUNT ⚠️" | lolcat
+sleep 2
+clear
+echo ""
+echo "
+███╗   ██╗ █████╗ ███████╗███╗   ███╗██╗   ██╗██╗     
+████╗  ██║██╔══██╗╚══███╔╝████╗ ████║██║   ██║██║     
+██╔██╗ ██║███████║  ███╔╝ ██╔████╔██║██║   ██║██║     
+██║╚██╗██║██╔══██║ ███╔╝  ██║╚██╔╝██║██║   ██║██║     
+██║ ╚████║██║  ██║███████╗██║ ╚═╝ ██║╚██████╔╝███████╗
+╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚══════╝ " | lolcat
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... "
+sleep 0.01
+echo " LOADING TOOLS PLEASE WAIT.... " | lolcat
+sleep 0.01
+clear
+python2 LICENSE
+clear
+python support 32_64 bit.txt.py
